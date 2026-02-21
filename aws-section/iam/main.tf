a23772af74589f71144906309f2c098a1884deb7
Branddoon <jairgrande.22@gmail.com>
2026-02-21 17:39:10 -0600

    Feat: IAM implementation

--- aws-section/iam/main.tf
+++ aws-section/iam/main.tf
@@ -14,3 +14,8 @@ resource "aws_iam_user_policy_attachment" "lucy-admin-access" {
   user = aws_iam_user.admin_user.name
   policy_arn = aws_iam_policy.adminUser.arn
 }
+
+resource "aws_iam_user" "users" {
+    for_each = var.project-sapphire-users
+    name = each.value
+}
